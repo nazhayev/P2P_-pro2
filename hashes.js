c95ea456a06a32b1fd4677e495b6081fa96cf01a
nazhayev <makeda5678@gmail.com>
2026-07-29 20:16:50 +0300

    Delete hashes.js

--- hashes.js
+++
@@ -1,8 +0,0 @@
-// Хеши кодов доступа (коды в открытом виде НЕ хранятся)
-const VALID_CODE_HASHES = [];
-
-function verifyCode(code) {
-    // Демо-режим: проверяем формат кода
-    const pattern = /^P2P-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}$/;
-    return pattern.test(code.trim().toUpperCase());
-}
